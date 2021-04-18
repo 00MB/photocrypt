5aaab3e3fddc9f37e8f1abaf80ce42b293045356
Angel-Shah <angel.shah2468@gmail.com>
2021-04-18 02:21:36 -0400

    all user login stuff

--- app.py
+++ app.py
@@ -2,8 +2,24 @@ from flask import Flask, render_template, request
 from convert import ascifii
 from hashlib import sha256
 import os
+import pymongo 
+from pymongo import MongoClient
+from datetime import datetime
+import json 
+from bson import json_util
+from bson.objectid import ObjectId
+
+
+cluster = MongoClient("mongodb+srv://Angel:dailyTasksAppPass@cluster0.a5gk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = cluster["PhotoCrypt-DB"]
+collection = db["UserCredentials"]
+
+
 
 app = Flask(__name__)
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,8 +31,16 @@ def index():
         ascii_string = ascifii()
         os.remove("pure.jpg")
         encrypted_string = sha256(ascii_string.encode()).hexdigest()
-        print(encrypted_string)
-        return render_template('index.html', encrypted_string = encrypted_string)
+
+        existingUser = list(collection.find({'username':username}))
+        # print(existingUser['password'])
+        if(existingUser):
+            if(existingUser[0]['password'] == encrypted_string):
+                return render_template('landingPage.html')
+        else:
+            return render_template('index.html', invalid = True)
+        # print(encrypted_string)
+        return render_template('index.html')
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -28,4 +52,18 @@ def register():
         ascii_string = ascifii()
         os.remove("pure.jpg")
         encrypted_string = sha256(ascii_string.encode()).hexdigest()
+        exists = list(collection.find({'username':username}))
+        if(exists):
+            # print("Username is already taken")
+            return render_template('register.html', usernameTaken = True)
+        else:
+            collection.insert_one({"username":username, "password":encrypted_string})
+            return render_template('index.html',createdUserLogin=username)
+
+        # print("username: ", username, "$$ encrypted string: ", encrypted_string)
+
     return render_template('register.html')
+
+@app.route('/landingPage',methods=['GET','POST'])
+def landingPage():
+    return render_template('landingPage.html')

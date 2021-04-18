e970e1fe15ad8860f79e717f3e687df415e3d231
00MB <s2061204@ed.ac.uk>
2021-04-18 08:13:55 +0100

    change

--- app.py
+++ app.py
@@ -16,11 +16,7 @@ db = cluster["PhotoCrypt-DB"]
 collection = db["UserCredentials"]
 
 
-
 app = Flask(__name__)
-
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -33,7 +29,6 @@ def index():
         encrypted_string = sha256(ascii_string.encode()).hexdigest()
 
         existingUser = list(collection.find({'username':username}))
-        # print(existingUser['password'])
         if(existingUser):
             if(existingUser[0]['password'] == encrypted_string):
                 return render_template('landingPage.html')
@@ -41,8 +36,6 @@ def index():
                 return render_template('index.html', invalid = True)
         else:
             return render_template('index.html', invalid = True)
-        # print(encrypted_string)
-        return render_template('index.html')
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -56,16 +49,17 @@ def register():
         encrypted_string = sha256(ascii_string.encode()).hexdigest()
         exists = list(collection.find({'username':username}))
         if(exists):
-            # print("Username is already taken")
             return render_template('register.html', usernameTaken = True)
         else:
             collection.insert_one({"username":username, "password":encrypted_string})
             return render_template('index.html',createdUserLogin=username)
-
-        # print("username: ", username, "$$ encrypted string: ", encrypted_string)
 
     return render_template('register.html')
 
 @app.route('/landingPage',methods=['GET','POST'])
 def landingPage():
     return render_template('landingPage.html')
+
+@app.route('/fetch',methods=['GET'])
+def landingPage():
+    return "<h1>test</h1>"

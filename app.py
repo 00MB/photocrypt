0ee0c3111fa133aa2524864ac100540c5ea00930
00MB <s2061204@ed.ac.uk>
2021-04-18 02:12:43 +0100

    added sha256

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from convert import ascifii
+from hashlib import sha256
 
 app = Flask(__name__)
 
@@ -10,8 +11,16 @@ def index():
         image = request.files['file']
         image.save("pure.jpg")
         ascii_string = ascifii()
+        encrypted_string = sha256(ascii_string.encode()).hexdigest()
+        print(encrypted_string)
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.form.get("username", False) != False:
+        username = request.form['username']
+        image = request.files['file']
+        image.save("pure.jpg")
+        ascii_string = ascifii()
+        encrypted_string = sha256(ascii_string.encode()).hexdigest()
     return render_template('register.html')

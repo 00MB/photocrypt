66b457cef04138ef08d69ff2adf95e3c8c6f4171
00MB <s2061204@ed.ac.uk>
2021-04-18 10:54:40 +0100

    First working version extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,7 +2,6 @@ const thisForm = document.getElementById("form");
 thisForm.addEventListener("submit", async function (e) {
   e.preventDefault();
   const formData = new FormData(thisForm).entries();
-  document.getElementById("demo").innerHTML = "Hello World!";
   console.log(formData);
   const response = await fetch(
     "https://photocrypthack.herokuapp.com/https://www.vippng.com/png/detail/320-3209103_200-kb-download-tree-with-transparent-background-clipart.png/true/true/10",
@@ -12,7 +11,7 @@ thisForm.addEventListener("submit", async function (e) {
   );
 
   const result = await response.json();
-  console.log(result);
+  document.getElementById("demo").innerHTML = "Code: " + result.code;
 });
 // var button = document.getElementById("submit");
 // if (!button) {

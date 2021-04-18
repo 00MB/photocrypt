fb69732036d289ef6ae2fe2f6b30bcc05dbfd92e
00MB <s2061204@ed.ac.uk>
2021-04-18 11:28:02 +0100

    stable

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,26 +1,47 @@
 const thisForm = document.getElementById("form");
 thisForm.addEventListener("submit", async function (e) {
   e.preventDefault();
-  const formData = new FormData(thisForm).entries();
-  console.log(formData);
+  const formData = new FormData(thisForm);
+  var url = formData.get("url");
+  var chars = formData.get("chars");
+  if (chars != null) {
+    chars = "true";
+  } else {
+    chars = "false";
+  }
+  var caps = formData.get("caps");
+  if (caps != null) {
+    caps = "true";
+  } else {
+    chars = "false";
+  }
+  var length = formData.get("length");
+  console.log(
+    "https://photocrypthack.herokuapp.com/" +
+      url +
+      "/" +
+      caps +
+      "/" +
+      chars +
+      "/" +
+      length
+  );
   const response = await fetch(
-    "https://photocrypthack.herokuapp.com/https://www.vippng.com/png/detail/320-3209103_200-kb-download-tree-with-transparent-background-clipart.png/true/true/10",
+    "https://photocrypthack.herokuapp.com/" +
+      url +
+      "/" +
+      caps +
+      "/" +
+      chars +
+      "/" +
+      length,
     {
+      mode: "no-cors",
       method: "GET",
     }
   );
 
   const result = await response.json();
   document.getElementById("demo").innerHTML = "Code: " + result.code;
+  thisForm.reset();
 });
-// var button = document.getElementById("submit");
-// if (!button) {
-//   console.log("works");
-// }
-// button.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log("hey");
-//   const req = new XMLHttpRequest();
-//   const baseUrl =
-//     "https://photocrypthack.herokuapp.com/https://www.vippng.com/png/detail/320-3209103_200-kb-download-tree-with-transparent-background-clipart.png/true/true/10";
-// });
